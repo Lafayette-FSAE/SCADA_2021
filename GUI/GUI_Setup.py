e86ad3758fb160e9f29be6026ee88be753d526ff
Harrison Walker <walkerhd@lafayette.edu>
2021-03-25 03:28:48 -0400

    attempting to fix redis GUI integration

--- GUI/GUI_Setup.py
+++ GUI/GUI_Setup.py
@@ -349,7 +349,7 @@ class GUISetup(tk.Frame):
                 
             message = p.get_message()
 
-        # self.after(1000, self.getNewData)
+        self.after(1000, self.getNewData)
         ## call this method after 1s to refresh data
 
 
