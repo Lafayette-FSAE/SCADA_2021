65c7d49f95399008e0dfac71adf2081033bcd60d
Harrison Walker <walkerhd@lafayette.edu>
2021-04-01 02:25:13 -0400

    still testing USB stuff

---
+++ drivers/usb_driver_tests/libusb_interface/usb_test_new.py
@@ -0,0 +1,27 @@
+import sys, os
+#Importing the config file
+lib_path = '/usr/etc/scada'
+config_path = '/usr/etc/scada/config'
+#this is temporary, just for testing
+local_path = '../utils'
+
+sys.path.append(lib_path)
+sys.path.append(config_path)
+sys.path.append(local_path)
+
+import config
+
+import ctypes
+import pathlib
+
+if __name__ == "__main__":
+    # Load the shared library from this folder into ctypes
+    libname = pathlib.Path().absolute() / "usb7402_interface.so"
+    c_lib = ctypes.CDLL(libname)
+    # Set the return type to double
+    c_lib.readChannel.restype = ctypes.c_double
+
+
+channel = 0
+result = c_lib.readChannel(ctypes.c_uint8(channel))
+
