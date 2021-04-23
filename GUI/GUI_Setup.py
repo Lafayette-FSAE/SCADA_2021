4881e0f18c46bb56f868ccd0721b95c2ef73c3db
vasschrys <liachrys1@gmail.com>
2021-04-23 15:40:44 -0400

    addign reset button

--- GUI/GUI_Setup.py
+++ GUI/GUI_Setup.py
@@ -382,7 +382,8 @@ class GUISetup(tk.Frame):
                 return value
 
     def runProcess(self): 
-        subprocess.run(["sudo", "bash", "make"])
-        python = sys.executable
-        os.execl(python, python, * sys.argv)
+        pass
+        # subprocess.run(["sudo", "bash", "make"])
+        # python = sys.executable
+        # os.execl(python, python, * sys.argv)
 
