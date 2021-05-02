4ed13cefa36db388dc1a628b0c5e0049f8f4ebc5
irwinfrimpong <frimponi@lafayette.edu>
2021-05-02 19:16:24 -0400

    Created imu setup in utils and edited sorter

--- sorter/sorter.py
+++ sorter/sorter.py
@@ -11,10 +11,11 @@ sys.path.append(lib_path)
 sys.path.append(config_path)
 
 from drivers import driver
-import utils
-from utils import rtc_setup
 import config
 import redis
+import utils
+from utils import rtc_setup
+from utils import imu_setup
 
 #Setting Up I2C Bus Connection 
 bus = smbus.SMBus(3) 
@@ -31,65 +32,22 @@ SensorList = config.get('Sensors')
 
 last_sampled = {}
 sample_period = {}
+
 for key in config.get('Sensors'):
     sample_period[key] = SensorList.get(key).get('sample_period')
     last_sampled[key] = time.time()
-
-####### Methods to Configure BNO055 IMU #######
-onSetup = False  #Boolean var used to peform imu setup on startup
-
-def imu_reset():
-    #IMU IN CONFIG MODE
-    print("IMU defined constant: " + str(config.get('IMU_Config_Constants').get('CONFIG_MODE'))) 
-    driver.write('opr_mode_reg',config.get('IMU_Config_Constants').get('CONFIG_MODE'))
-    try:
-        driver.write('trigger_reg',0x20)
-    except OSError:
-        pass
-    time.sleep(0.7)
-    
-
-def imu_setup():
-    #Debuggin: 
-    opr_mode_reg_read = driver.read('opr_mode_reg')
-    # print("Value of Opr_Mode: " + str(opr_mode_reg_read))
-   # print( "SensorList Dictionary: " + str(SensorList))
-    global onSetup # Python UnboundLocalError fix
-   # if (opr_mode_reg_read == 0 or opr_mode_reg_read != 12): #If its in Config Mode and not in NDOF mode, want to configure it
-    if (opr_mode_reg_read == 0 or (bool(onSetup) == False)):
-        onSetup = True #OnSetup has been achieved 
-        imu_reset()
-        driver.write('power_reg',config.get('IMU_Config_Constants').get('POWER_NORMAL'))
-        driver.write('page_reg',0x00)
-        driver.write('trigger_reg',0x00)
-        driver.write('acc_config_reg',config.get('IMU_Config_Constants').get('ACCEL_4G'))
-        driver.write('gyro_config_reg',config.get('IMU_Config_Constants').get('GYRO_2000_DPS'))
-        driver.write('mag_config_reg',config.get('IMU_Config_Constants').get('MAGNETOMETER_20HZ'))
-        time.sleep(0.01)
-    
-        ##Setting IMU TO NDOF MODE
-        driver.write('opr_mode_reg',config.get('IMU_Config_Constants').get('NDOF_MODE'))
-        time.sleep(0.7)
-
-## Methods to Configure the Pi time to RTC on boot
-def rtc_pitimesteup():
-    clk_id = time.CLOCK_REALTIME
-    t = time.clock_gettime(clk_id)
-
-    if (t < driver.read('rtc_time')): 
-        rtc_setup.set_RTCtime()
-        time.sleep(1)
 
 
 while True: 
     #for Sensors: <-- needs to be name of list of sensors
     # milliseconds = int(time()*1000)
 
-    ## IMU Setup
-    imu_setup()
+    ## RTC Startup Setup
+    rtc_setup.rtc_pitimesteup()
 
-    ## RTC Setup
-    rtc_pitimesteup()
+    ## IMU Startup Setup
+    imu_setup.imu_config()
+
 
     # Reading
     for sensorName in SensorList :
--- utils/rtc_setup.py
+++ utils/rtc_setup.py
@@ -11,7 +11,16 @@ sys.path.append(config_path)
 from drivers import driver
 import config 
 
+clk_id = time.CLOCK_REALTIME 
+
 def set_RTCtime():
 #Setting Raspberry Pi Time to RTC Time 
-    clk_id = time.CLOCK_REALTIME 
     time.clock_settime(clk_id, driver.read('rtc_time'))
+
+## Methods to Configure the Pi time to RTC on boot
+def rtc_pitimesteup():
+    t = time.clock_gettime(clk_id)
+
+    if (t < driver.read('rtc_time')): 
+        set_RTCtime()
+        time.sleep(1)
