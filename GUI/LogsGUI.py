ddf2137cc98aede0ed9d2ff9867bfa07f282d9af
vasschrys <liachrys1@gmail.com>
2021-05-07 00:44:51 -0400

    revert back to commit when I didnt add the redis log file to GUI

--- GUI/LogsGUI.py
+++ GUI/LogsGUI.py
@@ -3,12 +3,14 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk 
 import sys, os
+import psycopg2
+import redis
 config_path = '/usr/etc/scada/config'
 sys.path.append(config_path)
 
 database_path = '/usr/etc/scada/utils'
 sys.path.append(database_path)
-import config
+# import config
 import yaml
 import collections
 import time
@@ -19,6 +21,17 @@ from collections import defaultdict
 ## for reset button
 import subprocess as sub
 
+# creates instance of Redis
+redis_data = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# creates Publish/Subscribe Redis object called 'p'
+p = redis_data.pubsub()
+# p subscribes to get messages
+p.subscribe('logs')
+
+# create Postrgres database cursor
+#  a cursor is like a dummy user in a database that executes commands and retrieves results
+cursor = database.cursor()
 
 
 
@@ -50,9 +63,23 @@ class LogsGUI(tk.Frame):
         # for logs 
         # os.system('tail -n 100 /var/log/syslog | grep scada')
         p = sub.Popen(["sudo", "scada", "logs"],stdout=sub.PIPE, stderr=sub.PIPE)
-       # subprocess.check_call(["sudo", "scada", "logs"], cwd="/home/pi/SCADA_2021")
         output, errors = p.communicate()
         text = Text(self)
         text.grid(row = 0, column = 1, sticky= "w")
         text.insert(END, output)
         ## contents fo logs redis channel 
+
+        self.pollFromRedis()
+    
+
+    def pollFromRedis(self):
+        while True:
+            message = p.get_message() 
+            #print("message: " + str(message))
+            ## message = sensor:value
+            if (message and (message['data'] != 1 )):
+                [sensor_key, sensor_value] = self.splitMsg(message['data'])
+                logData = sensor_value
+                print(logData)
+    
+
